12121592926112fdc7b552fc2005b5792f6d3927
Matthias Vogel <m.vogel@andersundsehr.com>
2017-07-25 10:15:23 +0200

    [FEATURE] add sync.sh file for faster rebase php version branches

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git checkout master && git push origin master
+git checkout php7.1 && git rebase master && git push origin php7.1 -f
+git checkout php7.0 && git rebase master && git push origin php7.0 -f
+git checkout php5.6 && git rebase master && git push origin php5.6 -f
+git checkout master && git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit --all
