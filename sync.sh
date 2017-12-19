c9a2c3e980f02bce0e1d42f14b1189c54254c4d1
Matthias Vogel <m.vogel@andersundsehr.com>
2017-12-19 15:31:36 +0100

    [TASK] add php 7.2

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git checkout master && git push origin master
+git checkout php7.2 && git rebase master && git push origin php7.2 -f
 git checkout php7.1 && git rebase master && git push origin php7.1 -f
 git checkout php7.0 && git rebase master && git push origin php7.0 -f
 git checkout php5.6 && git rebase master && git push origin php5.6 -f
